40f9e62ca7e78d84da43acc29263de94c72c1c1e
AlexanderBaransky <alexander.baranskiy@yandex.ru>
2021-06-10 14:31:37 +0300

    backports: backports-include: linux: cred.h: Fix build error Caused because CONFIG_USER_NS missing

--- backports/backport-include/linux/cred.h
+++ backports/backport-include/linux/cred.h
@@ -4,7 +4,9 @@
 #include <linux/version.h>
 
 #ifndef current_user_ns
+#ifdef CONFIG_USER_NS
 #define current_user_ns()	(current->nsproxy->user_ns)
+#endif
 #endif
 
 #endif /* __BACKPORT_LINUX_CRED_H */
